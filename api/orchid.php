25edb8745959c5d6ec0b790dbca094192a5715c1
Natthawut Hemathulin <natthawut.nux@gmail.com>
2016-06-14 22:48:30 +0700

    Updated command to execute java command.

--- api/orchid.php
+++ api/orchid.php
@@ -1,8 +1,9 @@
 <?php
 header('content-type: application/json');
 
-$uploaddir = 'photos/';
-$uploadfile = $uploaddir . basename($_FILES['photo']['name']);
+$uploaddir = 'photos';
+$filename = basename($_FILES['photo']['name']);
+$uploadfile = $uploaddir . '/' . $filename;
 
 if (!move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)) {
     // Upload photo failed
@@ -12,8 +13,8 @@ if (!move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)) {
 }
 
 // TODO: Execute command
-$commandInput = "$uploadfile";
-$commandOutput = exec("pwd");
+$commandInput = 'java -cp ".;C:\Program Files\MATLAB\MATLAB Runtime\v901\toolbox\javabuilder\jar\javabuilder.jar;..\orchid-lib\CalEn.jar" ..\orchid-lib\TestCalEn '.$uploaddir.'\\'.$filename;
+$commandOutput = exec($commandInput);
 
 // Upload photo success
 http_response_code(200);
@@ -21,5 +22,3 @@ echo json_encode(['message' => 'File is valid, and was successfully uploaded.',
     'input' => $commandInput,
     'output' => $commandOutput
 ]);
-
-
