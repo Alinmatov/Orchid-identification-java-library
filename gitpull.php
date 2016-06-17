7c83520183c79d38593b6563fd91857187b7e5df
Natthawut Hemathulin <natthawut.nux@gmail.com>
2016-06-18 00:02:55 +0700

    Changed exec to system.

--- gitpull.php
+++ gitpull.php
@@ -2,11 +2,12 @@
 header('content-type: application/json');
 
 $commandInput = 'git pull';
-exec($commandInput, $commandOutput, $returnVar);
-
+// exec($commandInput, $commandOutput, $returnVar);
+$commandOutput = system($commandInput, $var);
 // Upload photo success
 http_response_code(200);
 echo json_encode([
     'input' => $commandInput,
     'output' => $commandOutput,
+    'var' => $var,
 ]);
