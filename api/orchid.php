6883ccd4e46c56f1648a56dbfc83dbd8ae5b086b
Natthawut Hemathulin <natthawut.nux@gmail.com>
2016-08-24 23:49:19 +0700

    Configure database.

--- api/orchid.php
+++ api/orchid.php
@@ -1,6 +1,18 @@
 <?php
 header('content-type: application/json');
 
+$servername = "localhost";
+$username = "orchid";
+$password = "orchid1234";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $uploaddir = 'photos';
 $filename = basename($_FILES['photo']['name']);
 $uploadfile = $uploaddir . '/' . $filename;
